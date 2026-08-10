2aa44290a2fe70ebfacccd3923fa7dd4cc3b8f0f
DuyLe1605 <leminhduy160505@gmail.com>
2026-08-10 14:28:36 +0700

    init(registration-service): registration with cross-service call

---
+++ init-db.sql
@@ -0,0 +1,3 @@
+-- Tạo 2 database cho 2 service
+CREATE DATABASE IF NOT EXISTS course_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS registration_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
